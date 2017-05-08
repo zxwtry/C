509889b9c356384a219391492909588d26148e9c
zxwtry <zxwtry@qq.com>
2017-05-09 07:12:31 +0800

    add leetcode/L131_PalindromePartitioning.cpp

---
+++ leetcode/L131_PalindromePartitioning.cpp
@@ -0,0 +1,27 @@
+#pragma warning(disable:4786)
+
+/*
+    url: leetcode.com/problems/palindrome-partitioning
+*/
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <queue>
+#include <set>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<string > > partition(string s) {
+        return NULL;    
+    }
+};
+
+int main() {
+    string s = "012345";
+    cout<<s.substr(4, 3)<<endl;;
+    return 0;
+}
